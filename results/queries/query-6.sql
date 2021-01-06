aa806dcfa40d4fea42fa313b8f80fd3a975e4bb5
Nawal Beitia <nawal.beitia@le-campus-numerique.fr>
2021-01-06 08:08:59 +0100

    query 6 test

--- results/queries/query-6.sql
+++ results/queries/query-6.sql
@@ -1,4 +1,3 @@
-SELECT name, price,products.quantity
-FROM order
-INNER JOIN products ON order_product.product_id = products.id
-WHERE order_product.order_id = 1;
+SELECT orders.id, (products.price*order_product.quantity)
+FROM orders join order_product ON orders.id = order_product.order_id 
+join products ON order_product.product_id = products.id
